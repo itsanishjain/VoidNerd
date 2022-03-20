e973bf2f985e3ddc3bbfe2f8d87afdec854ebaf4
anish jain <helloanishjain@gmail.com>
2022-03-20 21:03:45 +0530

    added NFTs render Compontent and hashlip arts

---
+++ Readme.md
@@ -0,0 +1,6 @@
+### Game Build using Smart contracts
+
+user can mint NFTs base on Unique Dna strore on the blockchian. Once user Own There NFTs 
+they can play with by providing them Various game items
+
+They can increate level of it 
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useContext,useState,useEffect } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { VoidNerdContext } from "../context/VoidNerd";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -7,23 +7,21 @@ import GradientButton from '../src/components/GradientButton'
 import Button from '../src/components/Button'
 import NFTCard from '../src/components/NFTCard'
 import Loader from '../src/components/Loader'
+import RenderVoidNerd from '../src/components/RenderVoidNerd'
 
 
 
 // ethers
 import { Contract, utils } from 'ethers'
-import { VOIDNERD_CONTRACT_ADDRESS,VOIDNERD_ABI } from '../src/constants';
+import { VOIDNERD_CONTRACT_ADDRESS, VOIDNERD_ABI } from '../src/constants';
 
 
 
 export default function Home() {
 
-  const { isWalletConnected, account, connectWallet, getSigner,shortenAddress,checkErrorTypeAndNotify,getOwnerVoidNerd,results,getLoading } = useContext(VoidNerdContext);
+  const { isWalletConnected, account, connectWallet, getSigner, shortenAddress, checkErrorTypeAndNotify, getOwnerVoidNerd, results, getLoading } = useContext(VoidNerdContext);
 
-  console.log("RESULTSSSSSSSSSSSSSs",results)
-
-
-  const [mintLoading,setMintLoading] = useState(false);
+  const [mintLoading, setMintLoading] = useState(false);
 
   // useEffect(()=>{
   //   getOwnerVoidNerd()
@@ -34,26 +32,26 @@ export default function Home() {
 
 
 
-    // Create the Void Nerd NFT
-    const createRandomVoidNerd = async (_name) => {
-      setMintLoading(true);
-      try{
-        const { signer } = await getSigner();
-        const voidNerdContract = new Contract(VOIDNERD_CONTRACT_ADDRESS,VOIDNERD_ABI,signer)
-        const tx = await voidNerdContract.createRandomVoidNerd(_name,{value:utils.parseEther('0.0001')});
-        await tx.wait();
-        await getOwnerVoidNerd()
-      }
-      catch(error) {
-        checkErrorTypeAndNotify(error);
-      }
-      setMintLoading(false);
+  // Create the Void Nerd NFT
+  const createRandomVoidNerd = async (_name) => {
+    setMintLoading(true);
+    try {
+      const { signer } = await getSigner();
+      const voidNerdContract = new Contract(VOIDNERD_CONTRACT_ADDRESS, VOIDNERD_ABI, signer)
+      const tx = await voidNerdContract.createRandomVoidNerd(_name, { value: utils.parseEther('0.0001') });
+      await tx.wait();
+      await getOwnerVoidNerd()
     }
+    catch (error) {
+      checkErrorTypeAndNotify(error);
+    }
+    setMintLoading(false);
+  }
 
   return (
     <div className="bg-gray-200 h-screen w-screen overflow-auto">
       <div className=" flex justify-end mx-4 ">
-        <GradientButton text={!isWalletConnected ? "Connect Wallet" : shortenAddress(account) } onClick={connectWallet} />
+        <GradientButton text={!isWalletConnected ? "Connect Wallet" : shortenAddress(account)} onClick={connectWallet} />
       </div>
 
       <div className="max-w-lg mx-auto p-2">
@@ -64,20 +62,26 @@ export default function Home() {
           </p>
         </div>
 
-        {!mintLoading ? <Button text="Mint" onClick={()=>createRandomVoidNerd("WAGMI")} /> : <Loader />}
+        {!mintLoading ? <Button text="Mint" onClick={() => createRandomVoidNerd("LFG")} /> : <Loader />}
 
 
-    
-        
       </div>
 
-      {!getLoading ? <div className="" >
-        {results && results.map((item,index)=>(
-          
-            <NFTCard  key = {index} id ={item.id} name={item.name} dna = {item.dna} rarity={item.rarity} level={item.level} />
-            ))}
- 
-            </div> : <Loader /> }
+      {
+        !getLoading ? (
+          <div className="mb-4" >
+            {
+              results && results.map((item, index) => (
+
+                <NFTCard key={index} id={item.id} name={item.name} dna={item.dna} rarity={item.rarity} level={item.level} />
+              ))}
+
+          </div>
+        ) : <Loader />
+
+      }
+
+
 
     </div>
   );
